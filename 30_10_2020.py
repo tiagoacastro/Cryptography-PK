fe78e8b0673f754b5478dab064ea058334d7cc59
Tiago Castro <tiagoaraujocastro@gmail.com>
2020-11-06 13:50:34 +0100

    fixes

--- 30_10_2020.py
+++ 30_10_2020.py
@@ -1,7 +1,6 @@
 import Crypto.Util.number as cu
 import hashlib as hl
 import math
-import random
 
 
 # Ex2
@@ -10,19 +9,19 @@ def RSAEncode (m, e, n):
     return pow(m, e, n)
 
 def RSADecode (c, d, n):
-    return pow(c, d, n)
+    return cu.long_to_bytes(pow(c, d, n))
 
-p = cu.getPrime(2048)
-q = cu.getPrime(1024)
+p = cu.getPrime(1536)
+q = cu.getPrime(512)
 n = p*q
 f = (p - 1) * (q - 1)
-e = random.randint(1, f)
+e = cu.getRandomNBitInteger(128)
 while e <= 1 or e >= f or cu.GCD(f, e) != 1:
     e = cu.getRandomNBitInteger(128)
 d = cu.inverse(e, f)
 
 cryptogram = RSAEncode(cu.bytes_to_long(bytearray(input("Input something\n").encode())), e, n)
-decoded = cu.long_to_bytes(RSADecode(cryptogram, d, n)).decode()
+decoded = RSADecode(cryptogram, d, n).decode()
 print(str(decoded))
 
 
@@ -32,7 +31,7 @@ def msgf1 (msg, len):
     num = cu.bytes_to_long(msg)
     output = 0b0
     for i in range(math.ceil(len/32)-1):
-        temp = hl.sha256(cu.long_to_bytes((num << i.bit_length()) + i))
+        temp = hl.sha256(cu.long_to_bytes((num << 32) + i))
         output = (output << temp.digest_size) + cu.bytes_to_long(temp.digest())
     return cu.long_to_bytes(output >> (output.bit_length() - len))
 
